d48ce70d07734827e148becf44ccb02ea70640db
51464 <73089610+51464@users.noreply.github.com>
2020-11-16 11:08:45 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,18 @@
+basic.showIcon(IconNames.EigthNote)
+let mode = 1
 basic.forever(function () {
-    basic.showIcon(IconNames.EigthNote)
-    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
+    if (mode == 0) {
+        if (pins.analogReadPin(AnalogPin.P0) > 200) {
+            basic.showIcon(IconNames.Yes)
+        } else {
+            basic.showIcon(IconNames.No)
+            music.playTone(988, music.beat(BeatFraction.Whole))
+            music.playTone(131, music.beat(BeatFraction.Whole))
+        }
+    } else {
+        led.plotBarGraph(
+        pins.analogReadPin(AnalogPin.P1),
+        1023
+        )
+    }
 })
